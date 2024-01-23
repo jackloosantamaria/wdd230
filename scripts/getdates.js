e7e024030fd1c969b82f6e370f532c21b5ddcb70
jackloosantamaria <jackloosantamaria@hotmail.com>
2024-01-22 19:05:41 -0500

    added hamburger icon to js

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -6,3 +6,13 @@ const choice = {
 };
 
 document.querySelector("#date").innerHTML = new Date().toLocaleDateString("en-US", choice);
+
+//Hamburger Button
+
+const hamButton = document.querySelector('#menu');
+const choices = document.querySelector('.choices');
+
+hamButton.addEventListener('click', () => {
+	choices.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
