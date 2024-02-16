2e674805ffee4866bf4a86eb69cea19e9c327aed
jackloosantamaria <jackloosantamaria@hotmail.com>
2024-02-16 11:15:14 -0500

    update js

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -53,11 +53,27 @@
 // }
 
 
-var temp= 57;
-var wSpeed= 15;
+
+//working js
+
+// var temp= 57;
+// var wSpeed= 15;
+// var windChill= (35.74 + (0.6215 * temp))-(35.75 * Math.pow(wSpeed,0.16)) + (0.4275*temp*Math.pow(wSpeed,0.16));
+
+// var windChill= Math.round(windChill);
+// document.getElementById("windchill").innerHTML= windChill;
+
+
+
+
+var temp= parseFloat(document.getElementById('temp').innerHTML);
+var wSpeed= parseFloat(document.getElementById('windspeed').innerHTML);
+if (temp <=50 && wSpeed >3){
 var windChill= (35.74 + (0.6215 * temp))-(35.75 * Math.pow(wSpeed,0.16)) + (0.4275*temp*Math.pow(wSpeed,0.16));
 
 var windChill= Math.round(windChill);
 document.getElementById("windchill").innerHTML= windChill;
-
-
+}
+else{
+    document.getElementById("windchill").innerHTML = "N/A";
+}
