3838e29758dc0fd4ef9503bde5545607f20bbfed
jackloosantamaria <jackloosantamaria@hotmail.com>
2024-02-18 09:41:30 -0500

    added js lazy load

---
+++ lesson07/scripts/getdates.js
@@ -0,0 +1,18 @@
+let newYear = new Date(document.lastModified);
+document.querySelector("#lastModified").textContent = newYear;
+
+const choice = {
+    year: "numeric"
+};
+
+document.querySelector("#date").innerHTML = new Date().toLocaleDateString("en-US", choice);
+
+//Hamburger Button
+
+const hamButton = document.querySelector('#menu');
+const choices = document.querySelector('.choices');
+
+hamButton.addEventListener('click', () => {
+	choices.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
