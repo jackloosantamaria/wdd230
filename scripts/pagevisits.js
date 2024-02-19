9add4604ffa5794eccf828d5b25d6358d1fb7d03
jackloosantamaria <jackloosantamaria@hotmail.com>
2024-02-19 14:16:20 -0500

    js updated

--- scripts/pagevisits.js
+++ scripts/pagevisits.js
@@ -2,7 +2,7 @@ const pageDisplay = document.querySelector(".visit");
 let arrayVisits = Number(window.localStorage.getItem("arrayVisits-ls")) || 0;
 
 if (arrayVisits !==0){
-    pageDisplay.textContent = `Number of Visits: ${arrayVisits}`;
+    pageDisplay.textContent = arrayVisits;
     }
 else{
     pageDisplay.textContent = `First Visit`;
@@ -12,25 +12,25 @@ arrayVisits++;
 
 localStorage.setItem("arrayVisits-ls", arrayVisits);
 
-document.addEventListener("ContentLoaded", function(){
-    const lastVisit = localStorage.getItem("lastVisit");
+// document.addEventListener("ContentLoaded", function(){
+//     const lastVisit = localStorage.getItem("lastVisit");
 
-    if(lastVisit){
-        const currentDate = Date.now();
-        const dateDifference = currentDate - parseInt(lastVisit);
-        const dayDifference = Math.floor(dateDifference / (1000 * 60 * 60 * 24));
+//     if(lastVisit){
+//         const currentDate = Date.now();
+//         const dateDifference = currentDate - parseInt(lastVisit);
+//         const dayDifference = Math.floor(dateDifference / (1000 * 60 * 60 * 24));
 
-        const message = document.getElementById("message");
+//         const message = document.getElementById("message");
 
-        if(dayDifference === 0){
-            message.textContent = "Welcome Back!"
-        }
-        else{
-            message.textContent = `Your last visit was ${dayDifference} days ago`;
-        }
-    }
-    else{
-        localStorage.setItem("lastVisit", Date.now());
-        document.getElementById("message").textContent = "Welcome!"
-    }
-});
+//         if(dayDifference === 0){
+//             message.textContent = "Welcome Back!"
+//         }
+//         else{
+//             message.textContent = `Your last visit was ${dayDifference} days ago`;
+//         }
+//     }
+//     else{
+//         localStorage.setItem("lastVisit", Date.now());
+//         document.getElementById("message").textContent = "Welcome!"
+//     }
+// });
