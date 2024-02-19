95d10ab6082e2f6c2d47c57a087c70682ca0fe34
jackloosantamaria <jackloosantamaria@hotmail.com>
2024-02-19 14:29:30 -0500

    updated js

--- scripts/pagevisits.js
+++ scripts/pagevisits.js
@@ -11,26 +11,3 @@ else{
 arrayVisits++;
 
 localStorage.setItem("arrayVisits-ls", arrayVisits);
-
-// document.addEventListener("ContentLoaded", function(){
-//     const lastVisit = localStorage.getItem("lastVisit");
-
-//     if(lastVisit){
-//         const currentDate = Date.now();
-//         const dateDifference = currentDate - parseInt(lastVisit);
-//         const dayDifference = Math.floor(dateDifference / (1000 * 60 * 60 * 24));
-
-//         const message = document.getElementById("message");
-
-//         if(dayDifference === 0){
-//             message.textContent = "Welcome Back!"
-//         }
-//         else{
-//             message.textContent = `Your last visit was ${dayDifference} days ago`;
-//         }
-//     }
-//     else{
-//         localStorage.setItem("lastVisit", Date.now());
-//         document.getElementById("message").textContent = "Welcome!"
-//     }
-// });
