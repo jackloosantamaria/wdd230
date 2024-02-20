a6bad36542c7e7393acf5bdf10ee42e278ca5f0d
jackloosantamaria <jackloosantamaria@hotmail.com>
2024-02-20 14:26:35 -0500

    update js date

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,11 +1,25 @@
-let newYear = new Date(document.lastModified);
-document.querySelector("#lastModified").textContent = newYear;
+// let newYear = new Date(document.lastModified);
+// document.querySelector("#lastModified").textContent = newYear;
 
-const choice = {
-    year: "numeric"
-};
+// const choice = {
+//     year: "numeric"
+// };
 
-document.querySelector("#date").innerHTML = new Date().toLocaleDateString('en-US', choice);
+// document.querySelector("#date").innerHTML = new Date().toLocaleDateString('en-US', choice);
+
+const newYear = document.querySelector("#date");
+const lastModification = document.querySelector("#lastModified");
+
+let currentYear = new Date().getFullYear();
+let lastModified = document.lastModified;
+
+newYear.innerHTML = `${currentYear}`;
+lastModification.innerHTML = `${lastModified}`;
+
+
+
+
+
 
 //Hamburger Button
 
