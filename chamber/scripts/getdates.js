11ca58635424f59c133e88c42e8916a1ff410fe6
jackloosantamaria <jackloosantamaria@hotmail.com>
2024-02-29 20:35:40 -0500

    hidden time js

--- chamber/scripts/getdates.js
+++ chamber/scripts/getdates.js
@@ -16,3 +16,19 @@ hamButton.addEventListener('click', () => {
 	choices.classList.toggle('open');
 	hamButton.classList.toggle('open');
 });
+
+//hidden timestamp
+
+
+const currentDate = new Date();
+  
+const mm = currentDate.getMonth() + 1;
+const dd = currentDate.getDate();
+const yy = currentDate.getFullYear();
+const hours = currentDate.getHours();
+const min = currentDate.getMinutes();
+const sec = currentDate.getSeconds();
+
+const newDateTime = `${mm.toString().padStart(2, '0')}-${dd.toString().padStart(2, '0')}-${yy} ${hours.toString().padStart(2, '0')}:${min.toString().padStart(2, '0')}:${sec.toString().padStart(2, '0')}`;
+
+document.getElementById('dhidden').value = newDateTime;
