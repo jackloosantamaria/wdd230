74302c23bbc683367145861e9e8791a9cc66f416
jackloosantamaria <jackloosantamaria@hotmail.com>
2024-03-05 16:51:10 -0500

    updated width

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -21,7 +21,7 @@ const displayCompanies = (companies) =>{
         logo.setAttribute("src", company.logo);
         logo.setAttribute("alt", `Logo of ${company.name}`);
         logo.setAttribute("loading", "lazy");
-        logo.setAttribute("width", "100");
+        logo.setAttribute("width", "150");
         logo.setAttribute("height", "100");
 
         cName.textContent = `${company.name}`;
