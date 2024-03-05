f7c619fb7fb22d2c24ce4b226e6511a02331e588
jackloosantamaria <jackloosantamaria@hotmail.com>
2024-03-05 16:51:55 -0500

    new width and height

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -21,8 +21,8 @@ const displayCompanies = (companies) =>{
         logo.setAttribute("src", company.logo);
         logo.setAttribute("alt", `Logo of ${company.name}`);
         logo.setAttribute("loading", "lazy");
-        logo.setAttribute("width", "150");
-        logo.setAttribute("height", "100");
+        logo.setAttribute("width", "200");
+        logo.setAttribute("height", "200");
 
         cName.textContent = `${company.name}`;
         cDescription.textContent = `${company.description}`;
