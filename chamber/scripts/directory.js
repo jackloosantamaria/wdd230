e6e08dfa42d88b0bcc39e3fab89d777d6f8f645e
jackloosantamaria <jackloosantamaria@hotmail.com>
2024-03-05 16:54:31 -0500

    changed it to auto format

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -21,8 +21,8 @@ const displayCompanies = (companies) =>{
         logo.setAttribute("src", company.logo);
         logo.setAttribute("alt", `Logo of ${company.name}`);
         logo.setAttribute("loading", "lazy");
-        logo.setAttribute("width", "200");
-        logo.setAttribute("height", "550");
+        logo.setAttribute("width", "auto");
+        logo.setAttribute("height", "auto");
 
         cName.textContent = `${company.name}`;
         cDescription.textContent = `${company.description}`;
