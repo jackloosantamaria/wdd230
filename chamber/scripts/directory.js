10f28c6c6b80c5599ebaf20823e9e8d62d119ba4
jackloosantamaria <jackloosantamaria@hotmail.com>
2024-03-06 09:45:08 -0500

    changing ratio to auto

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -21,8 +21,8 @@ const displayCompanies = (companies) =>{
         logo.setAttribute("src", company.logo);
         logo.setAttribute("alt", `Logo of ${company.name}`);
         //logo.setAttribute("loading", "lazy");
-        logo.setAttribute("width", "180");
-        logo.setAttribute("height", "135");
+        logo.setAttribute("width", "auto");
+        logo.setAttribute("height", "auto");
 
         cName.textContent = `${company.name}`;
         cDescription.textContent = `${company.description}`;
