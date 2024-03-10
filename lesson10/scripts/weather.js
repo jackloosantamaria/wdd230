0b88566fd585f6be151d40726e5b47ae2c858217
jackloosantamaria <jackloosantamaria@hotmail.com>
2024-03-10 17:03:28 -0500

    weather js

---
+++ lesson10/scripts/weather.js
@@ -0,0 +1,31 @@
+const currentTemp = document.querySelector("#current-temp");
+const weatherIcon = document.querySelector("weather-icon");
+const captionDesc = document.querySelector("figcaption");
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.74882974750255&lon=6.633323630731296&units=imperial&appid=d93c11a8f8e7a7283aed823d47647002';
+
+async function apiFetch(){
+    try{
+        const response = await fetch(url);
+        if (response.ok){
+            const data = await response.json();
+            console.log(data);
+            displayResults(data);
+        }
+        else{
+            throw Error(await response.text());
+        }
+    }catch(error){
+    }
+}
+
+function displayResults(data){
+    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute("src", iconsrc);
+    weatherIcon.setAttribute("alt", "weather icon");
+    captionDesc.textContent = `${desc}`;
+}
+
+apiFetch();
