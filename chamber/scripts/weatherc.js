4d3027df9b77ad5207f0851d41ff1bdf33518bc2
jackloosantamaria <jackloosantamaria@hotmail.com>
2024-03-12 17:14:27 -0500

    updating weather js

---
+++ chamber/scripts/weatherc.js
@@ -0,0 +1,152 @@
+//import {FtoC, MtoK, rundwill} from "./windchill.js";
+
+// const currentTemp = document.querySelector("#temp");
+// const weatherIcon = document.querySelector("#weather-icon");
+// const captionDesc = document.querySelector("figcaption");
+// const windSpeed = document.querySelector("#windspeed");
+
+// const url = 'https://api.openweathermap.org/data/2.5/weather?lat=8.986025739927062&lon=-79.51386897114354&units=metric&appid=d93c11a8f8e7a7283aed823d47647002';
+
+// if(currentTemp != null){
+//     async function apiFetch(){
+//         const response = await fetch(url);
+//         if (response.ok){
+//             const data = await response.json();
+//             //console.log(data);
+//             displayResults(data);
+//             }
+//         else{
+//             throw Error(await response.text());
+//             }
+       
+//     }
+
+
+//     function displayResults(data){
+        
+//         currentTemp.innerHTML = `${(data.main.temp).toFixed(1)}`;
+//         const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+//         const desc = data.weather[0].description;
+
+
+//         weatherIcon.setAttribute("src", iconsrc);
+//         weatherIcon.setAttribute("alt", "weather icon");
+//         captionDesc.textContent = `${desc}`;
+//         windSpeed.innerHTML = `${(data.wind.speed).toFixed(1)}`;
+       
+//         rundwill();
+//     }
+
+//     apiFetch();
+
+// }
+
+const currentTemp = document.querySelector("#temp");
+const weatherIcon = document.querySelector("#weather-icon");
+const captionDesc = document.querySelector("figcaption");
+const windSpeed = document.querySelector("#windspeed");
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=8.986025739927062&lon=-79.51386897114354&units=metric&appid=d93c11a8f8e7a7283aed823d47647002';
+
+async function apiFetch(){
+    try{
+        const response = await fetch(url);
+        if (response.ok){
+            const data = await response.json();
+            console.log(data);
+            displayResults(data);
+        }
+        else{
+            throw Error(await response.text());
+        }
+    }catch(error){
+    }
+}
+
+function displayResults(data){
+    currentTemp.innerHTML = `${data.main.temp}`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute("src", iconsrc);
+    weatherIcon.setAttribute("alt", "weather icon");
+    captionDesc.textContent = `${desc}`;
+    windSpeed.innerHTML = `${(data.wind.speed).toFixed(1)}`;
+    
+}
+
+//Forecast
+
+const forecastTemp1 = document.querySelector("#forecastTemp1");
+const weatherIcon1 = document.querySelector("#weatherIcon1");
+const figCap1 = document.querySelector("#fig2");
+const forecast1 = document.querySelector("#forecast1");
+
+const forecastTemp2 = document.querySelector("#forecastTemp2");
+const weatherIcon2 = document.querySelector("#weatherIcon2");
+const figCap2 = document.querySelector("#fig3");
+const forecast2 = document.querySelector("#forecast2");
+
+const forecastTemp3 = document.querySelector("#forecastTemp3");
+const weatherIcon3 = document.querySelector("#weatherIcon3");
+const figCap3 = document.querySelector("#fig4");
+const forecast3 = document.querySelector("#forecast3");
+
+const url1 = 'https://api.openweathermap.org/data/2.5/forecast?lat=8.986025739927062&lon=-79.51386897114354&units=metric&appid=d93c11a8f8e7a7283aed823d47647002';
+
+async function forecastApiFetch(){
+    try{
+        const response = await fetch(url1);
+        if(response.ok){
+            const data = await response.json();
+            console.log(data);
+            displayForecast(data);
+        }
+        else{
+            throw Error(await response.text());
+        }
+    }catch(error){
+        console.log(error);
+    }
+}
+
+function displayForecast(data){
+    forecastTemp1.innerHTML = `${data.list[6].main.temp.toFixed(1)}&deg;C`;
+    const iconsrc1 = `https://openweathermap.org/img/w/${data.list[6].weather[0].icon}.png`;
+    let desc1 = data.list[6].weather[0].description;
+    weatherIcon1.setAttribute("src", iconsrc1);
+    figCap1.textContent = `${desc1}`;
+
+    forecastTemp2.innerHTML = `${data.list[9].main.temp.toFixed(1)}&deg;C`;
+    const iconsrc2 = `https://openweathermap.org/img/w/${data.list[9].weather[0].icon}.png`;
+    let desc2 = data.list[9].weather[0].description;
+    weatherIcon2.setAttribute("src", iconsrc2);
+    figCap2.textContent = `${desc2}`;
+
+    forecastTemp3.innerHTML = `${data.list[19].main.temp.toFixed(1)}&deg;C`;
+    const iconsrc3 = `https://openweathermap.org/img/w/${data.list[19].weather[0].icon}.png`;
+    let desc3 = data.list[19].weather[0].description;
+    weatherIcon3.setAttribute("src", iconsrc3);
+    figCap3.textContent = `${desc3}`;
+}   
+
+function forecastDates(){
+    const daysOfWeek = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+    ];
+
+    const currentDate = new Date();
+    const todayIn = currentDate.getDay();
+
+    forecast1.innerHTML = `${daysOfWeek[(todayIn + 1) % 7]}`;
+}
+
+
+apiFetch();
+forecastDates();
+forecastApiFetch();
