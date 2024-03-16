6d8ddf7c1ab45dd2c88452e259f96dbcea9cae3b
jackloosantamaria <jackloosantamaria@hotmail.com>
2024-03-16 17:08:13 -0500

    updating dark mode

--- chamber/scripts/darkmode.js
+++ chamber/scripts/darkmode.js
@@ -1,11 +1,17 @@
+const modeButton = document.querySelector("#mode");
+const header = document.querySelector("header");
 const main = document.querySelector("#main-grid-box");
+const footer = document.querySelector("footer");
+const menu = document.querySelector(".choices");
 const body = document.querySelector("body");
 
 modeButton.addEventListener("click", () => {
     if (modeButton.textContent.includes("Dark Mode")){
         main.style.background = "#000";
         body.style.background = "#000";
-        
+        //header.style.background = "#000";
+        //footer.style.background = "#000";
+        // menu.style.background = "#000";
 
         modeButton.textContent = "Light Mode";
     }else{
