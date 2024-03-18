b41c4580cbb989bd86050bb33f4b29598f7bccbf
jackloosantamaria <jackloosantamaria@hotmail.com>
2024-03-17 20:08:05 -0500

    updating dark mode

--- chamber/scripts/darkmode.js
+++ chamber/scripts/darkmode.js
@@ -1,7 +1,12 @@
 const modeButton = document.querySelector("#mode");
 const main = document.querySelector("#main-grid-box");
-const body = document.querySelector("body");
+const body = document.querySelector("#body-index");
+const bodyDiscover = document.querySelector("#body-discover")
+const bodyDirectory = document.querySelector("#body-directory")
+const bodyJoin = document.querySelector("#join-body");
 const mainDiscover = document.querySelector("#discovery-main");
+const mainDirectory = document.querySelector("#directory-main");
+const mainJoin = document.querySelector("#main-join");
 
 modeButton.addEventListener("click", () => {
     if (modeButton.textContent.includes("Dark Mode")){
@@ -25,14 +30,50 @@ modeButton.addEventListener("click", () => {
 
 modeButton.addEventListener("click", () => {
     if (modeButton.textContent.includes("Dark Mode")){
-        body.style.background = "#000";
+        bodyDiscover.style.background = "#000";
         mainDiscover.style.background = "#000";
     
 
         modeButton.textContent = "Light Mode";
     }else{
-        body.style.background = "#fff";
+        bodyDiscover.style.background = "#fff";
         mainDiscover.style.background = "#fff";
+
+
+        modeButton.textContent = "Dark Mode"
+    }
+
+
+});
+
+modeButton.addEventListener("click", () => {
+    if (modeButton.textContent.includes("Dark Mode")){
+        bodyDirectory.style.background = "#000";
+        mainDirectory.style.background = "#000";
+    
+
+        modeButton.textContent = "Light Mode";
+    }else{
+        bodyDirectory.style.background = "#fff";
+        mainDirectory.style.background = "#fff";
+
+
+        modeButton.textContent = "Dark Mode"
+    }
+
+
+});
+
+modeButton.addEventListener("click", () => {
+    if (modeButton.textContent.includes("Dark Mode")){
+        bodyJoin.style.background = "#000";
+        mainJoin.style.background = "#000";
+    
+
+        modeButton.textContent = "Light Mode";
+    }else{
+        bodyJoin.style.background = "#fff";
+        mainJoin.style.background = "#fff";
 
 
         modeButton.textContent = "Dark Mode"
