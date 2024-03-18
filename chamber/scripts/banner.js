438fd67b953f4f4ceb0ac4996e0e963ef8319c64
jackloosantamaria <jackloosantamaria@hotmail.com>
2024-03-18 18:38:58 -0500

    updating filter membership

--- chamber/scripts/banner.js
+++ chamber/scripts/banner.js
@@ -10,3 +10,41 @@ function closeBanner(){
     document.getElementById("banner").style.display = "none";
     
 }
+
+///////////////////Membership/////////////////////////////
+
+const memberUrl = "https://jackloosantamaria.github.io/wdd230/chamber/data/members.json"
+
+async function getMembership(){
+    const response = await fetch(memberUrl);
+    const data = await response.json();
+    filterMember(data.companies);
+}
+
+const filterMember = (members) =>{
+    const premiumMembers = members.filter(member => member.membership === 'Silver' || member.membership === 'Gold');
+
+    const choosingMembers = premiumMembers.sort(() => 0.5 - Math.random()).slice(0,Math.floor(Math.random()*2) + 2);
+
+    const container = document.getElementById("companies");
+    container.innerHTML = "";
+
+    choosingMembers.forEach(member => {
+        const memberHTML = `
+            <section class="box company">
+                <img src="${member.logo}" alt="${member.name}">
+                <h2>${member.name}</h2>
+                <p>${member.description}</p>
+                <p>${member.address}</p>
+                <p><a href="tel:${member.phone}">${member.phone}</a></p>
+                <p><a href="${member.url}" target="_blank">${member.url}</a></p>
+            </section>
+        `;
+
+        container.innerHTML += memberHTML;
+    });
+
+    
+}
+
+getMembership();
