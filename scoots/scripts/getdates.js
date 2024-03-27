3b9c44cc687769e72b4f97fc533ce433e5cec1d0
jackloosantamaria <jackloosantamaria@hotmail.com>
2024-03-27 14:59:35 -0500

    creating weather in js

--- scoots/scripts/getdates.js
+++ scoots/scripts/getdates.js
@@ -48,3 +48,102 @@ function updateSlider(){
 }
 
 setInterval(nextSlide, 3000);
+
+//Weather info
+
+
+
+
+
+const currentTemp = document.querySelector("#temp");
+const weatherIcon = document.querySelector("#weather-icon");
+const captionDesc = document.querySelector("figcaption");
+const humidity = document.querySelector("#hum");
+
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=20.50619779894648&lon=-86.94570287731085&units=metric&appid=d93c11a8f8e7a7283aed823d47647002';
+
+async function apiFetch(){
+    try{
+        const response = await fetch(url);
+        if (response.ok){
+            const data = await response.json();
+            console.log(data);
+            displayResults(data);
+        }
+        else{
+            throw Error(await response.text());
+        }
+    }catch(error){
+    }
+}
+
+function displayResults(data){
+    currentTemp.innerHTML = `${data.main.temp}`;
+	humidity.innerHTML = `${data.main.humidity}`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute("src", iconsrc);
+    weatherIcon.setAttribute("alt", "weather icon");
+    captionDesc.textContent = `${desc}`;
+    
+    
+}
+
+//Forecast
+
+const forecastTemp1 = document.querySelector("#forecastTemp1");
+const weatherIcon1 = document.querySelector("#weatherIcon1");
+const figCap1 = document.querySelector("#fig2");
+const forecast1 = document.querySelector("#forecast1");
+const humidity1 = document.querySelector("#humi");
+
+const url1 = 'https://api.openweathermap.org/data/2.5/forecast?lat=20.50619779894648&lon=-86.94570287731085&units=metric&appid=d93c11a8f8e7a7283aed823d47647002';
+
+async function forecastApiFetch(){
+    try{
+        const response = await fetch(url1);
+        if(response.ok){
+            const data = await response.json();
+            console.log(data);
+            displayForecast(data);
+        }
+        else{
+            throw Error(await response.text());
+        }
+    }catch(error){
+        console.log(error);
+    }
+}
+
+function displayForecast(data){
+    forecastTemp1.innerHTML = `${data.list[7].main.temp.toFixed(1)}&deg;C`;
+	humidity1.innerHTML = `${data.list[7].main.humidity}`;
+    const iconsrc1 = `https://openweathermap.org/img/w/${data.list[7].weather[0].icon}.png`;
+    let desc1 = data.list[6].weather[0].description;
+    weatherIcon1.setAttribute("src", iconsrc1);
+    figCap1.textContent = `${desc1}`;
+
+}   
+
+function forecastDates(){
+    const daysOfWeek = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+    ];
+
+    const currentDate = new Date();
+    const todayIn = currentDate.getDay();
+
+    forecast1.innerHTML = `${daysOfWeek[(todayIn + 1) % 7]}`;
+
+}
+
+apiFetch();
+forecastDates();
+forecastApiFetch();
